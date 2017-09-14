25e468c506ab4c60a58543a0ec15b2a8048a7967
manasranjanbpd <899902+manasranjanbpd@users.noreply.github.com>
2017-09-14 21:23:49 +0530

    adding latest files

---
+++ FairylandChocolate/Models/ProductDetails.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace FairylandChocolate.Models
+{
+    public class ProductDetails
+    {
+        public ProductDetails()
+        {
+        }
+        public int ProductId
+        {
+            get;
+            set;
+        }
+		public string ProductName
+		{
+			get;
+			set;
+		}
+        public decimal Price
+		{
+			get;
+			set;
+		}
+		public string Size
+		{
+			get;
+			set;
+		}
+        public decimal Weight
+		{
+			get;
+			set;
+		}
+        public int NoOfChocolates
+		{
+			get;
+			set;
+		}
+        public string ProductInfo
+		{
+			get;
+			set;
+		}
+
+
+	}
+}
---
+++ FairylandChocolate/Scripts/Custom.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+//ProductDetails/Index?id=24567890
+$('div[id^=product-]').on('click',function(){
+var id = $(this).attr('id');
+//var productId=id.split('-')[1];
+var dataAction = $(this).attr('data-action');
+//dataAction = dataAction + "?id="+productId;
+window.location.href = dataAction;
+});
+});
